edaf1ee221f0129fc8f58bde4fa39b74e8232e86
joanvasquez21 <joanvrivas.21@gmail.com>
2024-09-06 18:15:06 +0200

    feat: the removed method is added

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -44,15 +44,32 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+   async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where : { id: id}
+    });
+    if(!product){
+      throw new NotFoundException(`Product with id #${id} not found`);
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    return this.product.update({
+      where : { id: id},
+      data: updateProductDto
+    })
+    
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.product.delete({
+      where: {id: id}
+    })
   }
 }
